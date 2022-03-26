2db9e348a43cac94ca33115b95f16daa5b2b275d
npgeorge <17250377+pcsubirachs@users.noreply.github.com>
2022-03-25 18:10:37 -0700

    heroku db file

--- web_app/db_create.py
+++ web_app/db_create.py
@@ -1,3 +1,8 @@
+# this allows us to run a command in the heroku cli that will create our db
+# go to heroku cli
+# type in python db_create.py
+# fin
+
 from web_app.models import db
 
 db.create_all()
